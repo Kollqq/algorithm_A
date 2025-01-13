16d32bee1c86109b683e01619a9c19e647339d7d
Anton <115146037+Kollqq@users.noreply.github.com>
2025-01-13 21:01:30 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,85 +1,134 @@
-import pygame
+class Node():
+    def __init__(self, parent=None, position=None):
+        self.parent = parent
+        self.position = position
+        self.g = 0
+        self.h = 0
+        self.f = 0
 
-EMPTY, WALL, ROUTE, START_POINT, END_POINT = 0, 5, 3, 2, 4
-TILE_SIZE, GRID_DIMENSION = 30, 20
+    def __eq__(self, other):
+        return self.position == other.position
 
-def find_path(grid_map, start_pos, end_pos):
-    open_nodes = [start_pos]
-    visited = set()
-    parent = {}
 
-    while open_nodes:
-        current_pos = open_nodes.pop(0)
+def astar(maze, start, end):
+    start_node = Node(None, start)
+    start_node.g = start_node.h = start_node.f = 0
+    end_node = Node(None, end)
+    end_node.g = end_node.h = end_node.f = 0
 
-        if current_pos == end_pos:
-            return reconstruct_path(parent, start_pos, end_pos)
+    open_list = []
+    closed_set = set()
 
-        visited.add(current_pos)
+    open_list.append(start_node)
 
-        for nx, ny in get_neighbors(grid_map, current_pos):
-            if (nx, ny) not in visited and grid_map[nx][ny] != WALL:
-                visited.add((nx, ny))
-                open_nodes.append((nx, ny))
-                parent[(nx, ny)] = current_pos
+    max_iterations = len(maze) * len(maze[0]) * 10
+    iterations = 0
 
-    return None
+    while len(open_list) > 0:
+        iterations += 1
+        if iterations > max_iterations:
+            print("Błąd: przekroczono limit iteracji. Droga prawdopodobnie jest niedostępna.")
+            return None
 
-def reconstruct_path(parent, start_pos, end_pos):
-    path = []
-    current_pos = end_pos
-    while current_pos != start_pos:
-        path.append(current_pos)
-        current_pos = parent[current_pos]
-    path.append(start_pos)
-    return path[::-1]
+        current_node = open_list[0]
+        current_index = 0
+        for index, item in enumerate(open_list):
+            if item.f < current_node.f:
+                current_node = item
+                current_index = index
 
-def get_neighbors(grid_map, position):
-    x, y = position
-    potential_neighbors = [(x + dx, y + dy) for dx, dy in [(-1, 0), (1, 0), (0, -1), (0, 1)]]
-    return [(nx, ny) for nx, ny in potential_neighbors if 0 <= nx < GRID_DIMENSION and 0 <= ny < GRID_DIMENSION]
+        open_list.pop(current_index)
+        closed_set.add(current_node.position)
 
-def load_map(file_path):
-    with open(file_path) as file:
-        return [list(map(int, line.split())) for line in file]
+        if current_node.position == end_node.position:
+            path = []
+            current = current_node
+            while current is not None:
+                path.append(current.position)
+                current = current.parent
+            return path[::-1]
 
-def save_map(grid_map, file_path):
-    with open(file_path, 'w') as file:
-        for row in grid_map:
-            file.write(' '.join(map(str, row)) + '\n')
+        children = []
+        for new_position in [(0, -1), (0, 1), (-1, 0), (1, 0), (-1, -1), (-1, 1), (1, -1), (1, 1)]:
+            node_position = (current_node.position[0] + new_position[0], current_node.position[1] + new_position[1])
 
-def display_path(grid_map, found_path):
-    pygame.init()
-    display = pygame.display.set_mode((GRID_DIMENSION * TILE_SIZE, GRID_DIMENSION * TILE_SIZE))
-    color_mapping = {EMPTY: (255, 255, 255), WALL: (0, 0, 0), ROUTE: (0, 255, 0), START_POINT: (128, 0, 128), END_POINT: (255, 255, 0)}
+            if node_position[0] > (len(maze) - 1) or node_position[0] < 0 or node_position[1] > (len(maze[0]) - 1) or node_position[1] < 0:
+                continue
 
-    for path_x, path_y in found_path:
-        grid_map[path_x][path_y] = ROUTE
+            if maze[node_position[0]][node_position[1]] != 0:
+                continue
 
-    while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                return
+            new_node = Node(current_node, node_position)
+            children.append(new_node)
 
-        for row_idx in range(GRID_DIMENSION):
-            for col_idx in range(GRID_DIMENSION):
-                rect = pygame.Rect(col_idx * TILE_SIZE, row_idx * TILE_SIZE, TILE_SIZE, TILE_SIZE)
-                pygame.draw.rect(display, color_mapping.get(grid_map[row_idx][col_idx], (200, 200, 200)), rect)
-                pygame.draw.rect(display, (200, 200, 200), rect, 1)
+        for child in children:
+            if child.position in closed_set:
+                continue
 
-        pygame.display.update()
+            child.g = current_node.g + 1
+            child.h = ((child.position[0] - end_node.position[0]) ** 2) + ((child.position[1] - end_node.position[1]) ** 2)
+            child.f = child.g + child.h
 
-grid_data = load_map('grid.txt')
-start_cell, end_cell = (0, 0), (19, 16)
+            if any(open_node for open_node in open_list if child == open_node and child.g >= open_node.g):
+                continue
 
-if grid_data[start_cell[0]][start_cell[1]] == WALL or grid_data[end_cell[0]][end_cell[1]] == WALL:
-    print("Początek lub cel znajduje się na przeszkodzie!")
-else:
-    grid_data[start_cell[0]][start_cell[1]], grid_data[end_cell[0]][end_cell[1]] = START_POINT, END_POINT
-    found_path = find_path(grid_data, start_cell, end_cell)
-    if found_path:
-        display_path(grid_data, found_path)
-        save_map(grid_data, 'final_grid.txt')
-        print("Końcowa mapa została zapisana w 'final_grid.txt'.")
+            open_list.append(child)
+
+
+def load_maze_from_file(file_name):
+    maze = []
+    with open(file_name, 'r') as f:
+        for line in f:
+            row = list(map(int, line.strip().split()))
+            maze.append(row)
+    return maze
+
+
+def print_maze(maze):
+    for row in maze:
+        print(" ".join(str(cell) for cell in row))
+
+
+def mark_path_on_maze(maze, path):
+    for position in path:
+        x, y = position
+        maze[x][y] = 3
+
+
+def save_maze_to_file(maze, file_name):
+    with open(file_name, 'w') as f:
+        for row in maze:
+            f.write(" ".join(map(str, row)) + '\n')
+
+
+def main():
+    maze = load_maze_from_file('grid.txt')
+
+    if len(maze) != 20 or len(maze[0]) != 20:
+        print("Błąd: mapa musi mieć rozmiar 20x20!")
+        return
+
+    start = (0, 0)
+    end = (7, 8)
+
+    if maze[start[0]][start[1]] != 0:
+        print("Błąd: punkt początkowy znajduje się na przeszkodzie.")
+        return
+
+    if maze[end[0]][end[1]] != 0:
+        print("Błąd: punkt końcowy znajduje się na przeszkodzie.")
+        return
+
+    path = astar(maze, start, end)
+    if path:
+        mark_path_on_maze(maze, path)
+        print("Końcowa mapa z zaznaczoną ścieżką:")
+        print_maze(maze)
+        save_maze_to_file(maze, 'grid_f.txt')
+        print("Końcowa mapa została zapisana w pliku 'grid_f.txt'.")
     else:
-        print("Nie znaleziono ścieżki!")
+        print("Nie znaleziono ścieżki.")
+
+
+if __name__ == '__main__':
+    main()
